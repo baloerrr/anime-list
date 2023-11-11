341efe6c4988da0355cac80c71d4227a16a0d194
baloerrr <99787895+baloerrr@users.noreply.github.com>
2023-11-12 03:45:15 +0700

    feat(search): Add Input Search Functionality

--- src/components/Navbar/InputSearch.tsx
+++ src/components/Navbar/InputSearch.tsx
@@ -1,7 +1,29 @@
+'use client'
+
+import { MagnifyingGlass } from '@phosphor-icons/react'
+import { useRef } from 'react'
+import { useRouter } from 'next/navigation'
+
 export default function InputSearch() {
-    return (
-        <div>
-            <input type="text" placeholder='Cari anime....' />
-        </div>
-    )
+  const searchRef = useRef<HTMLInputElement>(null)
+  const router = useRouter()
+
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    router.push(`/search/${searchRef.current?.value}`)
+  }
+
+  return (
+    <form className="relative" onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="Cari anime...."
+        className="w-full p-2 rounded"
+        ref={searchRef}
+      />
+      <button type='submit' className="absolute top-2 end-2">
+        <MagnifyingGlass size={24} />
+      </button>
+    </form>
+  )
 }
