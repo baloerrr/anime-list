fbe27a34025ead47013efa1a8d3fe859113230a3
baloerrr <99787895+baloerrr@users.noreply.github.com>
2023-11-19 19:01:35 +0700

    fix(pagination) : Fix validation of pangination

--- src/components/Utilities/Pagination.tsx
+++ src/components/Utilities/Pagination.tsx
@@ -1,33 +1,53 @@
 interface PaginationProps {
-    page: number,
-    setPage: any,
-    lastPage: number
+  page: number
+  setPage: any
+  lastPage: number
 }
 
-export default function Pagination({page, setPage, lastPage} : PaginationProps) {
+export default function Pagination({
+  page,
+  setPage,
+  lastPage,
+}: PaginationProps) {
+  const handleScrollTop = () => {
+    scrollTo({
+      behavior: 'smooth',
+      top: 0,
+    })
+  }
 
-    const handleScrollTop = () => {
-        scrollTo({
-            behavior: "smooth",
-            top: 0
-        })
-    }
+  const handleNextPage = () => {
+    setPage((prevState: number) => prevState + 1)
+    handleScrollTop()
+  }
 
-    const handleNextPage = () => {
-        setPage((prevState :number) => prevState + 1)
-        handleScrollTop()
-    }
+  const handlePrevPage = () => {
+    setPage((prevState: number) => prevState - 1)
+    handleScrollTop()
+  }
 
-    const handlePrevPage = () => {
-        setPage((prevState :number) => prevState - 1)
-        handleScrollTop()
-    }
+  return (
+    <div className="flex justify-center items-center px-2 py-4 gap-4 text-color-primary text-2xl">
+      {page <= 1 ? null : (
+        <button
+          onClick={handlePrevPage}
+          className="hover:text-color-accent transition-all"
+        >
+          Prev
+        </button>
+      )}
+      <p>
+        {page} of {lastPage}
+      </p>
 
-    return (
-        <div className="flex justify-center items-center px-2 py-4 gap-4 text-color-primary text-2xl">
-            <button onClick={handlePrevPage} className="hover:text-color-accent transition-all">Prev</button>
-            <p>{page} of {lastPage}</p>
-            <button onClick={handleNextPage} className="hover:text-color-accent transition-all">Next</button>
-        </div>
-    )
+      {page >= lastPage ? null : (
+        <button
+          onClick={handleNextPage}
+          className="hover:text-color-accent transition-all"
+        >
+          Next
+        </button>
+      )}
+    </div>
+  )
 }
